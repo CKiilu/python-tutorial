1eca6466deeaf69649f4fed021d43817bfa4db73
CKiilu <cndeti@gmail.com>
2016-02-03 11:10:16 +0300

    Polymorphism seen

--- 02_Quick Start/myoop2.py
+++ 02_Quick Start/myoop2.py
@@ -6,39 +6,52 @@ class Animal():
 	def move(self): return self.action['move']
 	def make_sound(self): return self.action['sound']
 	def sleep_time(self): return self.action['time']
-	def keeping_warm(self): return self.action['for warmth']
+	def keeping_warm(self): return self.action['warmth']
 """Humans class"""
 class Human(Animal):
 	action = dict(
 		move = "Bipedal walking.",
-		make_sound = "Speaking words.",
-		sleep_time = "At night.",
-		keeping_warm = "Wearing clothes."
+		sound = "Speaking words.",
+		time = "At night.",
+		warmth = "Wearing clothes."
 		)
 """Cat class"""
 class Cat(Animal):
 	action = dict(
 		move = "Quadrupedal walking.",
-		make_sound = "Meow!",
-		sleep_time = "All day.",
-		keeping_warm = "Fur."
+		sound = "Meow!",
+		time = "All day.",
+		warmth = "Fur."
 		)
 """Lion class"""
 class Lion(Animal):
 	action = dict(
 		move = "Quadrupedal walking.",
-		make_sound = "Roar!",
-		sleep_time = "All day.",
-		keeping_warm = "Fur."
+		sound = "Roar!",
+		time = "All day.",
+		warmth = "Fur."
 		)
 """Things happening in the savanna"""
-def savanna(Lion):
-	print(Lion.make_sound())
-	print(Lion.sleep_time())
+def savanna(lion):
+	print(lion.make_sound())
+	print(lion.sleep_time())
 """Things happening in the house"""
-def house(Human):
-	print(Human.make_sound())
-	print(Human.sleep_time())
-	print(Cat.make_sound())
-	print(Cat.sleep_time())
+def house(human):
+	print(human.make_sound())
+	print(human.sleep_time())
 """Calling the funtions"""
+def main():
+	Juma = Human()
+	Maria = Human()
+	Fluffy = Lion()
+	white_cat = Cat()
+
+	print("In the savanna: ")
+	for x in (Fluffy, Maria):
+		savanna(x)
+
+	print("In the house: ")
+	for y in (Maria, white_cat):
+		house(y)
+
+if __name__ == '__main__':	main()
