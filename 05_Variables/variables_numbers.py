a7839ea3b72505b82529e21aa01223133849e4bb
CKiilu <cndeti@gmail.com>
2016-02-03 15:55:55 +0300

    Number types

---
+++ 05_Variables/variables_numbers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+ 
+ # There are only two number types in python
+def main():
+	"""Number types"""
+	# 42; the answer to life, the universe and anything
+	x = 42
+	print(type(x), x)
+	# type (class = int)
+	y = 44.4
+	print(type(y), y)
+	# type (class = float)
+	other()
+
+def other():
+	"""Other useful functions"""
+
+if __name__ == "__main__":
+	main()
