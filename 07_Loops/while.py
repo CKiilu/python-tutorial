470db8ec1f787eb12c3d9fa0cedd9b26100a9799
CKiilu <cndeti@gmail.com>
2016-02-05 07:18:39 +0300

    while loops

---
+++ 07_Loops/while.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def main():
+	"""Simple fibonacci series"""
+	a, b = 0, 1
+	while b < 56:
+		print(b, end = " ")
+		a, b = b, a + b
+	print()
+
+if __name__ == "__main__":
+	main()
