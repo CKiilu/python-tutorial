a8ac8ab717db51ff365a64fd7a7f8be3b4c3b790
CKiilu <cndeti@gmail.com>
2016-02-08 08:00:48 +0300

    User input validation

---
+++ first_projects/usrinput.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def main():
+	sentence = input("Input sentence.")
+	words = sentence.split()
+	test = open('words.txt').readlines()
+	test_file = list(map(lambda x : x.strip(), test))
+	for x in words:
+		if x in test_file:
+				continue
+		else:
+			print(x)
+if __name__ == "__main__":
+	main()
